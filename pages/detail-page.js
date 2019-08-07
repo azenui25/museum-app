93da578c304a230b56030c862259dfc85f6dfbe3
azenui25 <asanga.nicholas@yahoo.com>
2019-08-07 11:36:41 +0200

    Create elements with data from form

--- pages/detail-page.js
+++ pages/detail-page.js
@@ -1,3 +1,25 @@
+// add event handler
 function submitComment() {
-    console.log('Hello!')
+    //gather data
+    const inputField = document.getElementById('name')
+    console.log(inputField)
+    const name = inputField
+    console.log(name)
+    const textArea = document.getElementById('msg')
+    const msg = textArea.value
+    console.log(msg)
+
+    // create the elements you need
+    const comment = document.createElement('section')
+    const h3 = document.createElement('h3')
+    const p = document.createElement('p')
+
+    // adjust the element we created
+    h3.innerHTML = $ { name }
+    said:
+        p.innerHTML = msg
+    comment.classList.add('comment')
+    comment.appendChild(h3)
+    comment.appendChild(p)
+    console.log(comment)
 }
