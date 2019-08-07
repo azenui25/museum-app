d3629e552cad331dd833aeb2ce61269460b5c6cd
azenui25 <asanga.nicholas@yahoo.com>
2019-08-07 13:37:55 +0200

    Set Up Form Validation

--- pages/detail-page.js
+++ pages/detail-page.js
@@ -1,34 +1,52 @@
 // add event handler
 function submitComment() {
-    //gather data
+    // gather data
     const inputField = document.getElementById('name')
-    console.log(inputField)
-    const name = inputField
-    console.log(name)
+    const name = inputField.value
     const textArea = document.getElementById('msg')
     const msg = textArea.value
-    console.log(msg)
 
     // create the elements you need
     const comment = document.createElement('section')
     const h3 = document.createElement('h3')
     const p = document.createElement('p')
 
-    // adjust the element we created
-    h3.innerHTML = `${name}`
-    said:
-        p.innerHTML = msg
+    // adjust the elements we created
+    h3.innerHTML = `${name} said:`
+    p.innerHTML = msg
     comment.classList.add('comment')
     comment.appendChild(h3)
     comment.appendChild(p)
-    console.log(comment)
 
-    // display the element on the page 
-    const commentSection = document.getElementById('comment')
-    console.log(commentSection)
+    // display the elements on the page
+    const commentSection = document.getElementById('comments')
     commentSection.appendChild(comment)
 
     // reset form values
     inputField.value = null
     textArea.value = null
+}
+
+function doesNotPassAllValidations(name, msg) {
+    if (!name || !msg) {
+        alert('You forgot to fill in your name or message!')
+        return true;
+    }
+
+    if (msg.length > 280) {
+        alert('Your comment is too long')
+        return true
+    }
+
+    return false
+}
+
+function submitComment() {
+    // ... gather data logic ..
+
+    // check if user input passes validations
+    if (doesNotPassAllValidations(name, msg)) {
+        return null
+    }
+
 }
